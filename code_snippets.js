89c99f693a9f4783cd32fb7b18bf1f58c0e1270f
kofi arhin <kofiarhin69@gmail.com>
2025-06-11 17:27:13 +0100

    Update code_snippets.js

--- code_snippets.js
+++ code_snippets.js
@@ -1,3 +1,41 @@
+//get the birthday of popular actors
+const API_KEY = 'YOUR_TMDB_API_KEY';
+const BASE_URL = 'https://api.themoviedb.org/3';
+
+async function getActorsBornToday() {
+  const today = new Date();
+  const todayMonthDay = `${String(today.getMonth() + 1).padStart(2, '0')}-${String(today.getDate()).padStart(2, '0')}`;
+
+  const popularUrl = `${BASE_URL}/person/popular?api_key=${API_KEY}`;
+
+  const response = await fetch(popularUrl);
+  const data = await response.json();
+
+  const results = [];
+
+  for (const person of data.results) {
+    const detailUrl = `${BASE_URL}/person/${person.id}?api_key=${API_KEY}`;
+    const detailRes = await fetch(detailUrl);
+    const detailData = await detailRes.json();
+
+    const birthday = detailData.birthday;
+    if (birthday && birthday.slice(5) === todayMonthDay) {
+      results.push({
+        name: detailData.name,
+        birthday: birthday,
+        known_for: detailData.known_for_department
+      });
+    }
+  }
+
+  console.log(results);
+  return results;
+}
+
+getActorsBornToday().catch(console.error);
+
+
+
 const kbJson = {
   name: "DevBot",
   languages: ["JavaScript", "Python"],
